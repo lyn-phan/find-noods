cbc92ce2b597f66b498e948f7541e0fb22441243
Lynda Phan <lynda.kim.phan@gmail.com>
2021-03-28 22:31:02 +0000

    Successfully added Google Maps API

--- server.py
+++ server.py
@@ -2,15 +2,16 @@ from flask import Flask, render_template, redirect, jsonify, request
 import jinja2
 import requests
 import os
+import module 
+import json
 
 app = Flask(__name__)
 app.secret_key = 'sendNoodsNow'
 # app.jinja_env.undefined = StrictUndefined
 
-
-foursquare_client_id = os.environ['CLIENT_ID']
-foursquare_client_secret = os.environ['CLIENT_SECRET'] 
-google_api_key = os.environ['GOOG_KEY']
+# foursquare_client_id = os.environ['CLIENT_ID']
+# foursquare_client_secret = os.environ.get['CLIENT_SECRET'] 
+# google_api_key = os.environ.get['GOOG_KEY']
 
 #### NAVIGATION ####
 @app.route('/')
@@ -18,48 +19,70 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/home', methods=['GET', 'POST'])
+def geocode_map():
+    """geocode map and grab lat/long"""
+
+    street_add = request.form.get("address")
+    address = " ".join([street_add, "Alameda, CA"]) 
+
+    url= "https://maps.googleapis.com/maps/api/geocode/json"
+    payload = {"address": address, "key": module.get_api_key("GOOG_KEY")}
+    req = requests.get(url, params=payload)
+    res = req.json()
+    print(res)
+    result = res['results'][0]
+    
+    lat_lng = []
+    lat = result["geometry"]["location"]["lat"]
+    lng = result["geometry"]["location"]["lng"]
+    lat_lng.append(lat)
+    lat_lng.append(lng)
+    lat_long = str(lat_lng)
+    
+    return lat_long
+
+
+@app.route('/home')
 def homepage():
-    if request.method == 'GET':
-        return render_template('home.html')
+    return render_template('home.html')
+
     
-    elif request.method == 'POST':
-        city = request.form.get('city') #grabbed user input and stored it in 'city' and 'query'
-        query = request.form.get('query')
-
-        url = "https://api.foursquare.com/v2/venues/search"
-        payload = {'near': city,
-                'query': query}
-        
-        r = requests.get(url, params=payload)
-
-        result = json.loads(r.request(url, 'GET')[1])
-        if result['response']['venues']:
-            restaurant = result['response']['venues'][0]
-            venue_id = restaurant['id']
-            restaurant_name = restaurant['name']
-            restaurant_address = restaurant['location']['formattedAddress']
-
-            """format address into one string"""
-            address = ""
-            for i in restaurant_address:
-                address += i + " "
-            restaurant_address = address 
-        
-        restaurantInfo = {'name': restaurant_name, 'address': restaurant_address}
-
-        return restaurantInfo
+@app.route('/home', methods = ['POST'])
+def homepage_query():
+    city = request.form.get('city') #grabbed user input and stored it in 'city' and 'query'
+    query = request.form.get('query')
     
-    else:
-        return "No restaurants found"
+    latlng = geocode_map()
+    foursq_id = module.get_4sq_id('CLIENT_ID')
+    foursq_secret = module.get_4sq_secret('CLIENT_SECRET')
 
-        # data = {'query': query, 'city': city} #created a dictionary
+    url = "https://api.foursquare.com/v2/venues/search"
+    payload = {'near': city, 'll': latlng, 'query': query, 'client_id': foursq_id, 'client_secret': foursq_secret}
+    r = requests.get(url, params=payload)
 
-        # return jsonify(data)
-         
-        #grab the data from POST form, input it into Foursquare API request
-        # return first restaurant that fulfills that search
+    print("-------------------------------------------")
+    print("this API call worked")
 
+    result = json.loads(r.request(url, 'GET')[1])
+
+    if result['response']['venues']:
+        restaurant = result['response']['venues'][0]
+        venue_id = restaurant['id']
+        restaurant_name = restaurant['name']
+        restaurant_address = restaurant['location']['formattedAddress']
+
+        """format address into one string"""
+        address = ""
+        for i in restaurant_address:
+            address += i + " "
+        restaurant_address = address 
+    
+    restaurantInfo = {'name': restaurant_name, 'address': restaurant_address}
+
+    return render_template("results.html", name=restaurant_name, address=restaurant_address)
+    
+    # else:
+    #     return "No restaurants found"
 
 
 if __name__ == '__main__':
